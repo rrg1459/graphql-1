93540f5194185245e0af15023f8c0f960443bae2
Rafael <rrodriguez@shokworks.io>
2022-02-06 10:09:58 -0400

    Remove variable  @spell_word

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,7 +3,6 @@ class GameController < ApplicationController
   def play
     @word = Diccionario.where.not(definicion: [nil, "", "No existe en la RAE"]).where(largo: (4..6)).sample
 
-    @spell_word = @word.nombre.split('')
     @array_word = Array.new(@word.largo)
 
   end
