751e9174c8ffae2da80a8bf2978deb306473dfe7
Rafael <rrodriguez@shokworks.io>
2022-02-07 08:44:54 -0400

    Clean code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,28 +21,63 @@ global.$ = jQuery;
 
 $(document).ready(function () {
 
+  document.getElementById("letter-0").focus();
+  document.getElementById("letter-0").setSelectionRange(1, 1);
   const word = $('#word').data('word');
+  // const xx = word.split
   const large = word.length;
   const letters = [];
+  const elem = document.activeElement;
 
-  console.log(large);
-  console.log(word);
+  console.log('elem', elem)
+  function validate(input) {
+    console.log('input: ', input)
+    const large = input.value.length - 1
+    input.value = input.value.replace(/[^abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ ]/g, '').substr(large, 1).toUpperCase();
+    input.setSelectionRange(1, 1);
+    recorre();
+    changeFocus();
+  }
+  window.validate = validate;
 
-  for (let step = 0; step < large; step++) {
-    let xx = $('#letter-' + step);
-    console.log(xx[0].value);
-    letters[step] = xx[0].value.toLowerCase();
+  function changeFocus() {
+    let num = Number(document.activeElement.id.split('-')[1]);
+    num == (large - 1) ? num = 0 : num++;
+    console.log('active: ', num);
+    console.log('active: ', large);
+    $('#letter-' + num).focus();
+    document.activeElement.setSelectionRange(1, 1);
   }
 
-  console.log(letters);
-  console.log(letters.join(''));
+  function toTheEnd(input) {
+    input.setSelectionRange(1, 1);
+  }
+  window.toTheEnd = toTheEnd;
+
+  console.log('large: ', large);
+  console.log('word: ', word);
+
+  function recorre() {
+    for (let step = 0; step < large; step++) {
+      let xx = $('#letter-' + step);
+      letters[step] = xx[0].value.toLowerCase();
+    }
+    console.log('letters: ', letters);
+    console.log('letters join: ', letters.join(''));
+  }
 
   $("#answer").click(function () {
     $(".hide-definition").fadeIn();
   });
 
-  $("#letter-1").click(function () {
-    console.log(this.value);
+  $("#reset").click(function () {
+    for (let step = 0; step < large; step++) {
+      $('#letter-' + step)[0].value = '';
+    }
+    document.getElementById("letter-0").focus();
   });
 
+});
+
+$(document).on('turbolinks:load', function () {
 });
