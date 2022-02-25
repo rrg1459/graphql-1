836fd97049919f44b4e71360da03a0d87da006ce
Rafael <rrodriguez@shokworks.io>
2022-02-25 13:41:39 -0400

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql-endpoint"
   end
-  post "/graphql", to: "graphql#execute"
+  post "/graphql-endpoint", to: "graphql#execute"
   root 'game#play'
   # root 'diccionarios#index'
   
