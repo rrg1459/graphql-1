aad454a036b6f8f1a31fc395e182217cd5d8d436
Rafael <rrodriguez@shokworks.io>
2022-02-25 14:15:41 -0400

    Remove -endpoint of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql-endpoint"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
-  post "/graphql-endpoint", to: "graphql#execute"
+  post "/graphql", to: "graphql#execute"
   root 'game#play'
   # root 'diccionarios#index'
   
